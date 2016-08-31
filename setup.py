0caa3040888f086ead7573bcdf60b6972118ca38
Peter Gerakios <peterg@focusschoolsoftware.com>
2016-08-30 20:07:56 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ezcolorizer',
+  packages = ['ezcolorizer'],
+  version = '2.0',
+  description = 'An easy way to color strings when printing output.',
+  author = 'The Portly Penguin',
+  author_email = 'admin@theportlypenguin.net',
+  url = 'https://github.com/the-portly-penguin/ezcolorizer',
+  download_url = 'https://github.com/the-portly-penguin/ezcolorizer.git',
+  keywords = ['script', 'output', 'color', 'text', 'string'],
+  classifiers = [],
+)
